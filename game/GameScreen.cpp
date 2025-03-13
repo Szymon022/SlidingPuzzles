dd53ffe9a36bc6d783ec9428f193336960cc886e
Szymon Kurpas <szymon.kurpas@nask.pl>
2025-03-13 14:59:02 +0100

    style tiles - empty tile + bigger, square tiles

--- game/GameScreen.cpp
+++ game/GameScreen.cpp
@@ -16,18 +16,26 @@ GameScreen::GameScreen(QWidget* parent) :
 {
     ui->setupUi(this);
 
-    int x[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
+    int x[3][3] = {{2, 8, 6}, {7, 1, 5}, {4, 3, 0}};
     this->board = new Board(x);
+    renderBoard();
 
     QPushButton* buttons[3][3] = {
         {ui->pushButton, ui->pushButton_2, ui->pushButton_3},
         {ui->pushButton_4, ui->pushButton_5, ui->pushButton_6},
         {ui->pushButton_7, ui->pushButton_8, ui->pushButton_9}
     };
+
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
         {
+            buttons[i][j]->setMinimumSize(200, 200);
+            buttons[i][j]->setMaximumSize(200, 200);
+            auto font = buttons[i][j]->font();
+            font.setPointSize(50);
+            font.setBold(true);
+            buttons[i][j]->setFont(font);
             connect(buttons[i][j], &QPushButton::clicked, this, [this, i, j] { onTileClick(i, j); });
         }
     }
@@ -50,7 +58,15 @@ void GameScreen::renderBoard()
     {
         for (int j = 0; j < this->board->getBoardCols(); j++)
         {
-            buttons[i][j]->setText(QString::number(board->getTile(i, j)));
+            int tile = board->getTile(i, j);
+            if (tile == 0)
+            {
+                buttons[i][j]->setText("");
+            }
+            else
+            {
+                buttons[i][j]->setText(QString::number(tile));
+            }
         }
     }
 }
