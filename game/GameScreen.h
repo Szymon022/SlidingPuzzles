3549a965fec9d38837747c9f4cf30e84307b0d1c
Szymon022 <szymon.kurpas00@gmail.com>
2025-03-13 12:26:06 +0100

    working 3x3 grid with correctly changing numbers

--- game/GameScreen.h
+++ game/GameScreen.h
@@ -5,18 +5,29 @@
 #ifndef GAMESCREEN_H
 #define GAMESCREEN_H
 
+#include <QPushButton>
 #include <QWidget>
+
+#include "Board.h"
 
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class GameScreen; }
+
+namespace Ui
+{
+    class GameScreen;
+}
+
 QT_END_NAMESPACE
 
-class GameScreen : public QWidget {
-Q_OBJECT
+class GameScreen : public QWidget
+{
+    Q_OBJECT
 
 public:
-    explicit GameScreen(QWidget *parent = nullptr);
+    explicit GameScreen(QWidget* parent = nullptr);
+    void onTileClick(int row, int column);
+    void renderBoard();
     ~GameScreen() override;
 
 private slots:
@@ -26,7 +37,8 @@ signals:
     void navigateToMainMenu();
 
 private:
-    Ui::GameScreen *ui;
+    Ui::GameScreen* ui;
+    Board* board;
 };
 
 
