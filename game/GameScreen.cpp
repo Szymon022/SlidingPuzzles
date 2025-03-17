85b0b96f711cbd4cc9a6cf712c39d4c88e8fc4ab
Szymon022 <szymon.kurpas00@gmail.com>
2025-03-17 08:21:24 +0100

    adapt GameScreen to new board implementation

--- game/GameScreen.cpp
+++ game/GameScreen.cpp
@@ -9,27 +9,35 @@
 #include <QStackedWidget>
 
 #include "ui_GameScreen.h"
+#include "board/tiles/EmptyTile.h"
+#include "board/tiles/NumberTile.h"
 
 
-GameScreen::GameScreen(QWidget* parent) :
-    QWidget(parent), ui(new Ui::GameScreen)
-{
+GameScreen::GameScreen(QWidget *parent) : QWidget(parent), ui(new Ui::GameScreen) {
     ui->setupUi(this);
 
-    int x[3][3] = {{2, 8, 6}, {7, 1, 5}, {4, 3, 0}};
-    this->board = new Board(x);
-    renderBoard();
+    this->board = new Board(
+        {
+            new NumberTile(1, 2),
+            new NumberTile(7, 8),
+            new NumberTile(5, 6),
+            new NumberTile(6, 7),
+            new NumberTile(0, 1),
+            new NumberTile(4, 5),
+            new NumberTile(3, 4),
+            new NumberTile(2, 3),
+            new EmptyTile(8),
+        }
+    );
 
-    QPushButton* buttons[3][3] = {
+    QPushButton *buttons[3][3] = {
         {ui->pushButton, ui->pushButton_2, ui->pushButton_3},
         {ui->pushButton_4, ui->pushButton_5, ui->pushButton_6},
         {ui->pushButton_7, ui->pushButton_8, ui->pushButton_9}
     };
 
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
             buttons[i][j]->setMinimumSize(200, 200);
             buttons[i][j]->setMaximumSize(200, 200);
             auto font = buttons[i][j]->font();
@@ -39,55 +47,45 @@ GameScreen::GameScreen(QWidget* parent) :
             connect(buttons[i][j], &QPushButton::clicked, this, [this, i, j] { onTileClick(i, j); });
         }
     }
+    renderBoard();
 }
 
-void GameScreen::onTileClick(int row, int column)
-{
+void GameScreen::onTileClick(int row, int column) {
     board->onTileClick(row, column);
     checkWinCondition();
     renderBoard();
 }
 
-void GameScreen::checkWinCondition() const
-{
-    if (board->isSolved())
-    {
+void GameScreen::checkWinCondition() const {
+    if (board->isSolved()) {
         qDebug() << "GAME WIN";
     }
 }
 
 
-void GameScreen::renderBoard()
-{
-    QPushButton* buttons[3][3] = {
+void GameScreen::renderBoard() {
+    QPushButton *buttons[3][3] = {
         {ui->pushButton, ui->pushButton_2, ui->pushButton_3},
         {ui->pushButton_4, ui->pushButton_5, ui->pushButton_6},
         {ui->pushButton_7, ui->pushButton_8, ui->pushButton_9}
     };
-    for (int i = 0; i < this->board->getBoardRows(); i++)
-    {
-        for (int j = 0; j < this->board->getBoardCols(); j++)
-        {
-            int tile = board->getTile(i, j);
-            if (tile == 0)
-            {
+    for (int i = 0; i < this->board->getSize(); i++) {
+        for (int j = 0; j < this->board->getSize(); j++) {
+            auto tile = board->getTile(i, j);
+            if (dynamic_cast<EmptyTile *>(tile) != nullptr) {
                 buttons[i][j]->setText("");
-            }
-            else
-            {
-                buttons[i][j]->setText(QString::number(tile));
+            } else if (auto numberTile = dynamic_cast<NumberTile *>(tile); numberTile != nullptr) {
+                buttons[i][j]->setText(QString::number(numberTile->getValue()));
             }
         }
     }
 }
 
-void GameScreen::onNavigateToMainMenu()
-{
+void GameScreen::onNavigateToMainMenu() {
     emit navigateToMainMenu();
 }
 
-GameScreen::~GameScreen()
-{
+GameScreen::~GameScreen() {
     delete board;
     delete ui;
 }
