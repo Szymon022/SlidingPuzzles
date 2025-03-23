2fb4ab6a29514e53f5e4fbc6e6d7eaa9ea42f22f
Szymon022 <szymon.kurpas00@gmail.com>
2025-03-23 18:11:42 +0100

    implement GameWonDialog

---
+++ game/dialog/GameWonDialog.cpp
@@ -0,0 +1,33 @@
+//
+// Created by szymon on 3/23/25.
+//
+
+// You may need to build the project (run Qt uic code generator) to get "ui_GameWonDialog.h" resolved
+
+#include "GameWonDialog.h"
+#include "ui_GameWonDialog.h"
+
+
+GameWonDialog::GameWonDialog(QWidget *parent) : QDialog(parent), ui(new Ui::GameWonDialog) {
+    ui->setupUi(this);
+}
+
+GameWonDialog::~GameWonDialog() {
+    delete ui;
+}
+
+void GameWonDialog::setBestMoves(const QString &bestMoves) const {
+    ui->bestMovesLabel->setText(bestMoves);
+}
+
+void GameWonDialog::setYourMoves(const QString &yourMoves) const {
+    ui->yourMovesLabel->setText(yourMoves);
+}
+
+void GameWonDialog::setBestTime(const QString &bestTime) const {
+    ui->bestTimeLabel->setText(bestTime);
+}
+
+void GameWonDialog::setYourTime(const QString &yourTime) const {
+    ui->yourTimeLabel->setText(yourTime);
+}
