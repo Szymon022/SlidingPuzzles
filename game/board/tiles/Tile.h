b1eabdd0c7b41fa0c1a1bb4ab2adf7050eab85dd
Szymon022 <szymon.kurpas00@gmail.com>
2025-04-01 19:15:05 +0200

    add comments for tiles

--- game/board/tiles/Tile.h
+++ game/board/tiles/Tile.h
@@ -6,11 +6,16 @@
 #define TILE_H
 #include <stdexcept>
 
-
+/**
+ * Base class representing board tile.
+ */
 class Tile {
     int ordinal;
 
 public:
+    /**
+     * @param ordinal ordinal of a tile in a board.
+     */
     explicit Tile(const int ordinal): ordinal(ordinal) {
         if (ordinal < 0) throw std::invalid_argument("ordinal must be >= 0");
     }
@@ -18,6 +23,12 @@ public:
     virtual ~Tile() {
     }
 
+    /**
+     * Returns ordinal of a tile in a board. Ordinal in a 2d board is an index of a tile in a board that is written in a 1d array row-wise.
+     * For 3x3 board ordinals are as follows: `[[0, 1, 2], [3, 4, 5], [6, 7, 8]]`, where inner arrays represents subsequent board rows.
+     *
+     * @return ordinal of a tile in a board.
+     */
     int getOrdinal() const {
         return ordinal;
     };
