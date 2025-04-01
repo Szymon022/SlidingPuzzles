69a251e5c697e55bbd35894d99176633372cf9b3
Szymon022 <szymon.kurpas00@gmail.com>
2025-04-01 20:14:21 +0200

    add game settings screen docs

--- settings/GameSettingsScreen.h
+++ settings/GameSettingsScreen.h
@@ -19,18 +19,38 @@ namespace Ui {
 
 QT_END_NAMESPACE
 
+/**
+ * Class representing game setting screen where user selects gama parameters: board size, enables/disable time limit, sets time limit.
+ */
 class GameSettingsScreen : public QWidget {
     Q_OBJECT
 
 signals:
+    /**
+     * Emitted if the user clicks return to main menu button.
+     * @param popBackStack if this view should be popped from the navigation stack
+     */
     void navigateToMainMenu(bool popBackStack);
 
+    /**
+     * Emitted if the user clicks start game button.
+     * @param boardSize board size that the player will solve
+     * @param timeLimitMs time limit. It is disabled unless enableTimeLimit flag is set to true.
+     * @param enableTimeLimit if the time limit should be enabled or not
+     */
     void navigateToGameScreen(int boardSize, int timeLimitMs, bool enableTimeLimit);
 
 private slots:
+    /**
+     * Handles preview board size update.
+     * @param boardSize board size that the preview should represent
+     */
     void onUpdateBoardPreviewSize(int boardSize) const;
 
 public:
+    /**
+     * @param parent parent of this screen.
+     */
     explicit GameSettingsScreen(QWidget *parent = nullptr);
 
     ~GameSettingsScreen() override;
