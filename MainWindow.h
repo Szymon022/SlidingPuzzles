94d6ee7f0ab261b0b7409caf1c34515ec46228bd
Szymon022 <szymon.kurpas00@gmail.com>
2025-04-01 20:07:02 +0200

    add ResultsStorage and MainWindow comments

--- MainWindow.h
+++ MainWindow.h
@@ -20,6 +20,9 @@ namespace Ui {
 
 QT_END_NAMESPACE
 
+/**
+ * Class representing the root of the whole ui - MainWindow. It encapsulates navigation logic.
+ */
 class MainWindow : public QMainWindow {
     Q_OBJECT
 
@@ -29,10 +32,23 @@ public:
     ~MainWindow() override;
 
 public slots:
+    /**
+     * Handles navigate to main menu.
+     * @param popBackStack if all the widgets from top of the stack to the MainMenu screen should be popped from the stacked.
+     */
     void handleNavigateToMainMenu(bool popBackStack);
 
+    /**
+     * Handles navigate to game screen.
+     */
     void handleNavigateToGameSettings();
 
+    /**
+     * Handles navigate to new game screen.
+     * @param boardSize size of the board that the player will solve
+     * @param timeLimit time limit. It is ignored unless enableTimeLimit is set to true
+     * @param enableTimeLimit enables time limit
+     */
     void handleNavigateToNewGame(int boardSize, int timeLimit, bool enableTimeLimit);
 
 private:
