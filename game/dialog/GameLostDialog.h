296427996cad44ec024f2c934efc9c542385b386
Szymon022 <szymon.kurpas00@gmail.com>
2025-04-01 19:30:45 +0200

    add dialog comments

--- game/dialog/GameLostDialog.h
+++ game/dialog/GameLostDialog.h
@@ -16,15 +16,28 @@ namespace Ui {
 
 QT_END_NAMESPACE
 
+/**
+ * Dialog that is shown to the user whe he/she loses the game. It is used in a time-limited games only.
+ */
 class GameLostDialog : public QDialog {
     Q_OBJECT
 
 signals:
+    /**
+     * Signal emitted when user clicks exit to main menu button.
+     */
     void exitToMainMenu();
 
+    /**
+     * Signal emitted when user clicks play again button.
+     */
     void playAgain();
 
 public:
+    /**
+     * Creates a game lost dialog.
+     * @param parent parent of the dialog.
+     */
     explicit GameLostDialog(QWidget *parent = nullptr);
 
     ~GameLostDialog() override;
--- game/dialog/GameWonDialog.h
+++ game/dialog/GameWonDialog.h
@@ -16,25 +16,54 @@ namespace Ui {
 
 QT_END_NAMESPACE
 
+/**
+ * Represents game won dialog. It shows user and best times and moves counter. It offers the user to play again or exit to main menu.
+ */
 class GameWonDialog : public QDialog {
     Q_OBJECT
 
 signals:
+    /**
+     * Emitted when user clicked exit to main menu button.
+     */
     void exitToMainMenu();
 
+    /**
+     * Emitted when user clicked play again button.
+     */
     void playAgain();
 
 public:
+    /**
+     * Creates GameWonDialog with placeholder information. Use appropriate set*() methods to set actual values.
+     * @param parent parent of this dialog
+     */
     explicit GameWonDialog(QWidget *parent = nullptr);
 
     ~GameWonDialog() override;
 
+    /**
+     * Sets label of the best moves section.
+     * @param bestMoves label to be shown in the best moves section.
+     */
     void setBestMoves(const QString &bestMoves) const;
 
+    /**
+     * Sets label of the user's moves counter section.
+     * @param yourMoves label to be shown in the user's moves section.
+     */
     void setYourMoves(const QString &yourMoves) const;
 
+    /**
+     * Sets label of the best time section.
+     * @param bestTime label to be shown in the best time section.
+     */
     void setBestTime(const QString &bestTime) const;
 
+    /**
+     * Sets label of the user's time section.
+     * @param yourTime label to be shown in the user's time section.
+     */
     void setYourTime(const QString &yourTime) const;
 
 private:
